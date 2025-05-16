967e65acfcd73e07baf59af197fe98571b5a1e57
clz2025-red <clz2025.red@gmail.com>
2025-05-16 14:54:18 +0900

    2025-05-16

---
+++ src/echo/Client.java
@@ -0,0 +1,57 @@
+package echo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+//뉴욕
+public class Client {
+
+	public static void main(String[] args) throws IOException{
+		
+		//소켓생성  종이컵전화기
+		Socket socket = new Socket();
+		
+		System.out.println("<클라이언트 시작>");
+		System.out.println("=================================================");
+		
+		//서버에 연결요청
+		// ip:192.168.0.96    port:10001
+		System.out.println("[서버에 연결을 요청합니다.]");
+		socket.connect(new InetSocketAddress("192.168.0.96", 10001));
+	
+		//쓰기 스트림 준비
+		//OutputStream out = new FileOutputStream("C:\\javaStudy\\MS949-copy.txt");
+		OutputStream out = socket.getOutputStream();  //주스트림
+		OutputStreamWriter osw = new OutputStreamWriter(out, "UTF-8");
+		BufferedWriter bw = new BufferedWriter(osw);
+	
+		//읽기 스트림 준비
+		InputStream in = socket.getInputStream(); //주스트림
+		InputStreamReader isr = new InputStreamReader(in, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+		
+		//메세지 보내기
+		bw.write("안녕");
+		bw.newLine();
+		bw.flush();
+		
+		//메세지 받기
+		String reMsg= br.readLine();
+		System.out.println("server:[" +   reMsg  + "]");
+		
+		System.out.println("======================================");
+		System.out.println("<클라이언트 종료>");
+		
+		//닫기
+		bw.close();
+		socket.close();
+	}
+
+}
